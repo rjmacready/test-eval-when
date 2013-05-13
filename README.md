81dc3e4d0dbccbeaae305287fc797367d65d0337
RJ MacReady <rjmacready@github.com>
2013-05-13 14:57:58 -0700

    removed conclusions from main to README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 
 Some ventures on `eval-when`.
+
+On emacs, `C-c` `C-k`
+prints
+   :COMPILE-TOPLEVEL
+   :LOAD-TOPLEVEL
+
+On sbcl, `(load "main.lisp")`
+prints
+   :EXECUTE
+
+On sbcl, `(compile-file "main.lisp")`
+prints
+   :COMPILE-TOPLEVEL
+
+
+On sbcl, `(load (compile-file "main.lisp"))`
+prints
+   :COMPILE-TOPLEVEL
+   :LOAD-TOPLEVEL
+
+On sbcl, `(load "main.fasl")`
+prints
+   :LOAD-TOPLEVEL
+
+After `(make-exe)`, calling executable
+prints
+   None :D
--- main.lisp
+++ main.lisp
@@ -1,31 +1,4 @@
 
-; On emacs, C-c C-k
-; prints
-;   :COMPILE-TOPLEVEL
-;   :LOAD-TOPLEVEL
-
-; On sbcl, (load "main.lisp")
-; prints
-;   :EXECUTE
-
-; On sbcl, (compile-file "main.lisp")
-; prints
-;   :COMPILE-TOPLEVEL
-
-
-; On sbcl, (load (compile-file "main.lisp"))
-; prints
-;   :COMPILE-TOPLEVEL
-;   :LOAD-TOPLEVEL
-
-; On sbcl, (load "main.fasl")
-; prints
-;   :LOAD-TOPLEVEL
-
-; After (make-exe), calling executable
-; prints
-;   None :D
-
 (eval-when (:compile-toplevel)
   (print :compile-toplevel))
 
